c63df95a1aafc2a27d21912a023acaca94912f8e
Ramon Massulo <72590597+Massul0@users.noreply.github.com>
2021-06-02 23:15:44 -0300

    Finish

---
+++ Flappy/js/teste.js
@@ -0,0 +1,9 @@
+// Verbo + Substantivo
+let corSite = "azul"
+function resetaCor(cor,tonalidade,tipo){
+    corSite = cor + ' ' + tonalidade + ' ' + tipo;
+};
+
+console.log(corSite);
+resetaCor("verde","claro","metalico");
+console.log(corSite);
